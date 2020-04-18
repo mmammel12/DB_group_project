a8011af893719e10a8582afb3ed9e1a1532f2282
mamammel <mamammel@iu.edu>
2020-04-18 10:41:10 -0700

    all base html added

--- UI/bottle_app.py
+++ UI/bottle_app.py
@@ -1,7 +1,41 @@
-from bottle import route, run
+from bottle import default_app, route, post, template, run
 
-@route('/hello')
-def hello():
-    return "Hello World!"
 
-run(host='localhost', port=8080, debug=True)
+@route("/")
+def home():
+    return template("index.html")
+
+
+@route("/view")
+def basicForm():
+    return template("view.html")
+
+
+@post("/basicResult")
+def basicResult():
+    return template("result.html")
+
+
+@route("/bbForm")
+def bbForm():
+    return template("bbForm.html")
+
+
+@post("/bbResult")
+def bbResult():
+    return template("bbResult.html")
+
+
+@route("/final")
+def final():
+    return template("final.html")
+
+
+@post("/finalResult")
+def finalResult():
+    return template("finalResult.html")
+
+
+application = default_app()
+
+run(host="localhost", port=8080, debug=True)
