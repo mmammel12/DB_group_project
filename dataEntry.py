e8a61a012eba9fb60ee605eb4146e3470505d3fd
mamammel <mamammel@iu.edu>
2020-04-19 14:23:49 -0700

    testing

--- dataEntry.py
+++ dataEntry.py
@@ -1,4 +1,5 @@
 import pyodbc
+import time
 
 server = 'IN-CSCI-MSSQL.ADS.IU.EDU\\SQLSERV2017DEV2' 
 database = 'Group7' 
@@ -7,9 +8,24 @@ password = 'Venwiv5-CSCI44300'
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
 
+start = time.time()
+cursor.execute('''
+    select
+        CityName,
+        StateProvName,
+        CountryName
+    from Cities, StatesProvs, Countries
+    where
+        Cities.StateProvID = StatesProvs.StateProvID AND
+        StatesProvs.CountryID = Countries.CountryID
+    order by
+        CountryName   desc,
+        StateProvName asc,
+        CityName      asc;'''
+    )
+end =  time.time()
+print(end-start)
 
-cursor.execute("SELECT * FROM Courses")
 
-
-for row in cursor:
-    print(row)
+# for row in cursor:
+#     print(row)
